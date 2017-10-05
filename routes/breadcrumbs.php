8329da53bd1571720ab78a3c51197e10b56b2d2e
Rey Hernandez <reyvhernandez@gmail.com>
2017-10-05 14:01:52 +0800

    rename home to dashboard

--- routes/breadcrumbs.php
+++ routes/breadcrumbs.php
@@ -1,5 +1,5 @@
 <?php
 
-Breadcrumbs::register('home', function ($breadcrumbs) {
-    $breadcrumbs->push('Home', '/', ['icon' => 'fa-dashboard']);
+Breadcrumbs::register('dashboard', function ($breadcrumbs) {
+    $breadcrumbs->push('Dashboard', '/dashboard', ['icon' => 'fa-dashboard']);
 });
