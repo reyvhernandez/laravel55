be4daee00de5d47e51455547a25bd67b5cdf931c
Rey Hernandez <reyvhernandez@gmail.com>
2017-10-06 09:18:33 +0800

    move user model to App/Models namespace

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,13 +1,14 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use HasRoles, Notifiable;
 
     /**
      * The attributes that are mass assignable.
